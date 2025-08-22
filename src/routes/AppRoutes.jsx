83afb4fe2e2e0ac71a49dbb9bd8793e982a185db
Mahmood Kaliika <mahmoodkaliika810@gmail.com>
2025-08-22 21:52:04 +0300

    Update and rename src/AppRoutes.jsx to src/routes/AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,10 +4,6 @@ import { Toaster } from 'react-hot-toast';
 
 import Home from '../pages/Home';
 import About from '../pages/About';
-import Services from '../pages/Services';
-import Blog from '../pages/Blog';
-import Contact from '../pages/Contact';
-import HelpCenter from '../pages/HelpCenter';
 import Privacy from '../pages/Privacy';
 import Terms from '../pages/Terms';
 import NotFound from '../pages/NotFound';
@@ -19,10 +15,6 @@ export default function AppRoutes() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/help-center" element={<HelpCenter />} />
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/terms" element={<Terms />} />
 
